28f77c6972477306dd903768faa25edfd29b14db
RajatSablok <rajat.main06@gmail.com>
2020-10-11 01:29:18 +0530

    commented node-schedule

--- app.js
+++ app.js
@@ -91,106 +91,107 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
 });
 
 //Schedule a job that runs every hour
-var j = schedule.scheduleJob("*/1 * * *", async function (fireDate) {
-  console.log("This job will run every hour");
+// var j = schedule.scheduleJob("*/1 * * *", async function (fireDate) {
+//   console.log("This job will run every hour");
 
-  let entitiesArr = [];
-  let companyArr = [];
+//   let entitiesArr = [];
+//   let companyArr = [];
 
-  const analyzeParams = {
-    url: "https://inshorts.com/en/read/business",
-    features: {
-      entities: {
-        limit: 200,
-        sentiment: true,
-      },
-    },
-  };
+//   const analyzeParams = {
+//     url: "https://inshorts.com/en/read/business",
+//     features: {
+//       entities: {
+//         limit: 200,
+//         sentiment: true,
+//       },
+//     },
+//   };
 
-  await naturalLanguageUnderstanding
-    .analyze(analyzeParams)
-    .then((analysisResults) => {
-      const entities = analysisResults.result.entities;
-      for (i in entities) {
-        if (entities[i].type == "Company" && entities[i].sentiment.score != 0) {
-          entitiesArr.push(entities[i]);
-        }
-      }
+//   await naturalLanguageUnderstanding
+//     .analyze(analyzeParams)
+//     .then((analysisResults) => {
+//       const entities = analysisResults.result.entities;
+//       for (i in entities) {
+//         if (entities[i].type == "Company" && entities[i].sentiment.score != 0) {
+//           entitiesArr.push(entities[i]);
+//         }
+//       }
 
-      for (i in companies) {
-        for (j in entitiesArr) {
-          if (
-            companies[i]["Company Name"].includes(entitiesArr[j].text) ||
-            companies[i]["Symbol"].includes(entitiesArr[j].text)
-          ) {
-            let obj = {};
-            obj.companyName = companies[i]["Company Name"];
-            obj.symbol = companies[i]["Symbol"];
-            obj.sentiment = entitiesArr[j].sentiment;
-            companyArr.push(obj);
-          }
-        }
-      }
-      // console.log(companyArr);
-    })
-    .catch((err) => {
-      console.log(err.toString());
-    });
+//       for (i in companies) {
+//         for (j in entitiesArr) {
+//           if (
+//             companies[i]["Company Name"].includes(entitiesArr[j].text) ||
+//             companies[i]["Symbol"].includes(entitiesArr[j].text)
+//           ) {
+//             let obj = {};
+//             obj.companyName = companies[i]["Company Name"];
+//             obj.symbol = companies[i]["Symbol"];
+//             obj.sentiment = entitiesArr[j].sentiment;
+//             companyArr.push(obj);
+//           }
+//         }
+//       }
+//       // console.log(companyArr);
+//     })
+//     .catch((err) => {
+//       console.log(err.toString());
+//     });
 
-  await User.find()
-    .then(async (users) => {
-      for (i in users) {
-        for (j in users[i].tracking) {
-          for (k in companyArr) {
-            if (
-              !users[i].tracking[j].mailSent &&
-              companyArr[k].companyName == users[i].tracking[j].companyName
-            ) {
-              const msg = {
-                to: users[i].email,
-                from: {
-                  email: process.env.SENDGRID_EMAIL,
-                  name: "EmoStock",
-                },
-                subject: "Stock Update",
-                text: `${companyArr[k].sentiment.label}`,
-                // html: EmailTemplates.VERIFY_EMAIL(result5),
-              };
+//   await User.find()
+//     .then(async (users) => {
+//       for (i in users) {
+//         for (j in users[i].tracking) {
+//           for (k in companyArr) {
+//             if (
+//               !users[i].tracking[j].mailSent &&
+//               companyArr[k].companyName == users[i].tracking[j].companyName
+//             ) {
+//               const msg = {
+//                 to: users[i].email,
+//                 from: {
+//                   email: process.env.SENDGRID_EMAIL,
+//                   name: "EmoStock",
+//                 },
+//                 subject: "Stock Update",
+//                 text: `${companyArr[k].sentiment.label}`,
+//                 // html: EmailTemplates.VERIFY_EMAIL(result5),
+//               };
 
-              await sgMail
-                .send(msg)
-                .then(async () => {
-                  users[i].tracking[j].mailSent = true;
-                  await users[i].save();
-                })
-                .catch((err) => {
-                  console.log(err.toString());
-                });
-            }
-          }
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err.toString());
-    });
-});
+//               // await sgMail
+//               //   .send(msg)
+//               //   .then(async () => {
+//               //     users[i].tracking[j].mailSent = true;
+//               //     await users[i].save();
+//               //   })
+//               //   .catch((err) => {
+//               //     console.log(err.toString());
+//               //   });
+//             }
+//           }
+//         }
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err.toString());
+//     });
+// });
 
 //Schedule a job that runs every hour
-var k = schedule.scheduleJob("*/1 * *", async function (fireDate) {
-  await User.find()
-    .then(async (users) => {
-      for (i in users) {
-        for (j in users[i].tracking) {
-          users[i].tracking[j].mailSent = false;
-          await users[i].save();
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err.toString());
-    });
-});
+// var k = schedule.scheduleJob("*/1 * *", async function (fireDate) {
+//   console.log("test");
+//   await User.find()
+//     .then(async (users) => {
+//       for (i in users) {
+//         for (j in users[i].tracking) {
+//           users[i].tracking[j].mailSent = false;
+//           await users[i].save();
+//         }
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err.toString());
+//     });
+// });
 
 const PORT = process.env.PORT || 5000;
 
